1d69b1cc65770a7b990ddf4ec72bfdc21d413af3
Allysa <allysagallardo@gmail.com>
2025-10-27 20:52:15 -0700

    able to display local mysql into the local host"

---
+++ database/db-connector.js
@@ -0,0 +1,19 @@
+// ./database/db-connector.js
+
+// Database
+// I got this error : Client does not support authentication protocol requested by server; consider upgrading MySQL client
+// so I used this recommendation: https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server/56509065#56509065 
+// and now it works yay
+var mysql = require('mysql2');
+
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : '127.0.0.1',
+    user            : 'root',
+    password        : 'ally0824!!',
+    database        : 'demo'
+});
+
+// Export it for use in our applicaiton
+module.exports.pool = pool;
